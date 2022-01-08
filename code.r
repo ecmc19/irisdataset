9c3b2b93d5fc20425e25929b78ccfb1c913cfef0
ecmc19 <90702979+ecmc19@users.noreply.github.com>
2022-01-08 00:28:43 +0000

    Update code.r

--- code.r
+++ code.r
@@ -28,6 +28,7 @@ boxplot(SS$Sepal.Length,
         SVI$Sepal.Length, 
         xlab = "Species", 
         ylab ="Lengths (cm)", 
+        main = "Sepal Width",
         las = 1, 
         names=c("Setosa", "Versicolor", "Virginica"))
 #range
@@ -47,6 +48,7 @@ hist(SVI$Sepal.Length, main = "Sepal Length: \n Virginica", xlab = "Length (cm)"
 
 
 ##################### Petal length analysis #####################
+par(mfrow = c(1,1))
 ##setosa
 PS <-subset(iris, subset=Petal.Length & Species == "setosa") %>%
      select(-Sepal.Width, -Sepal.Length, -Petal.Width)
@@ -57,18 +59,20 @@ PV <-subset(iris, subset=Petal.Length & Species == "versicolor") %>%
 PVI <-subset(iris, subset=Petal.Length & Species == "virginica") %>%
       select(-Sepal.Width, -Sepal.Length, -Petal.Width)
 #boxplot
+par(mfrow = c(1,1)) #fix previous code
 boxplot(PS$Petal.Length, 
         PV$Petal.Length, 
         PVI$Petal.Length, 
         xlab = "Species", 
-        ylab ="Lengths (cm)", 
+        ylab ="Lengths (cm)",
+        main = "Petal Length",
         las = 1, 
         names=c("Setosa", "Versicolor", "Virginica"))
 #range
 max(iris$Petal.Length)-min(iris$Petal.Length)
-max(PS$Petal.Length)-min(SS$Petal.Length) #setosa
-max(PV$Petal.Length)-min(SV$Petal.Length) #versicolor
-max(PVI$Petal.Length)-min(SVI$Petal.Length) #virginica
+max(PS$Petal.Length)-min(PS$Petal.Length) #setosa
+max(PV$Petal.Length)-min(PV$Petal.Length) #versicolor
+max(PVI$Petal.Length)-min(PVI$Petal.Length) #virginica
 #variance
 var(PS$Petal.Length) #setosa
 var(PV$Petal.Length) #versicolor
@@ -78,3 +82,73 @@ par(mfrow = c(1,3))
 hist(PS$Petal.Length, main = "Petal Length: \n Setosa", xlab = "Length (cm)")
 hist(PV$Petal.Length, main = "Petal Length: \n Versicolor", xlab = "Length (cm)")
 hist(PVI$Petal.Length, main = "Petal Length: \n Virginica", xlab = "Length (cm)")
+
+##################### Sepal width analysis #####################
+##setosa
+WS <-subset(iris, subset=Sepal.Width & Species == "setosa") %>%
+     select(-Sepal.Length, -Petal.Length, -Petal.Width)
+##versicolor
+WV <-subset(iris, subset=Sepal.Width  & Species == "versicolor") %>%
+     select(-Sepal.Length, -Petal.Length, -Petal.Width)
+##virginica
+WVI <-subset(iris, subset=Sepal.Width & Species == "virginica") %>%
+      select(-Sepal.Length, -Petal.Length, -Petal.Width)
+#boxplot
+par(mfrow = c(1,1)) #fix previous code
+boxplot(WS$Sepal.Width, 
+        WV$Sepal.Width, 
+        WVI$Sepal.Width, 
+        xlab = "Species", 
+        ylab ="Widths (cm)",
+        main = "Sepal Width",
+        las = 1, 
+        names=c("Setosa", "Versicolor", "Virginica"))
+#range
+max(iris$Sepal.Width)-min(iris$Sepal.Width)
+max(WS$Sepal.Width)-min(WS$Sepal.Width) #setosa
+max(WV$Sepal.Width)-min(WV$Sepal.Width) #versicolor
+max(WVI$Sepal.Width)-min(WVI$Sepal.Width) #virginica
+#variance
+var(WS$Sepal.Width) #setosa
+var(WV$Sepal.Width) #versicolor
+var(WVI$Sepal.Width) #virginica
+#histogram
+par(mfrow = c(1,3))
+hist(WS$Sepal.Width, main = "Sepal Width: \n Setosa", xlab = "Width (cm)")
+hist(WV$Sepal.Width, main = "Sepal Width: \n Versicolor", xlab = "Width (cm)")
+hist(WVI$Sepal.Width, main = "Sepal Width: \n Virginica", xlab = "Width (cm)")
+
+##################### Petal width analysis #####################
+##setosa
+WPS <-subset(iris, subset=Petal.Width & Species == "setosa") %>%
+     select(-Sepal.Length, -Petal.Length, -Sepal.Width)
+##versicolor
+WPV <-subset(iris, subset=Petal.Width  & Species == "versicolor") %>%
+     select(-Sepal.Length, -Petal.Length, -Sepal.Width)
+##virginica
+WPVI <-subset(iris, subset=Petal.Width & Species == "virginica") %>%
+      select(-Sepal.Length, -Petal.Length, -Sepal.Width)
+#boxplot
+par(mfrow = c(1,1)) #fix previous code
+boxplot(WPS$Petal.Width, 
+        WPV$Petal.Width, 
+        WPVI$Petal.Width, 
+        xlab = "Species", 
+        ylab ="Widths (cm)",
+        main = "Petal Width",
+        las = 1, 
+        names=c("Setosa", "Versicolor", "Virginica"))
+#range
+max(iris$Petal.Width)-min(iris$Petal.Width)
+max(WPS$Petal.Width)-min(WPS$Petal.Width) #setosa
+max(WPV$Petal.Width)-min(WPV$Petal.Width) #versicolor
+max(WPVI$Petal.Width)-min(WPVI$Petal.Width) #virginica
+#variance
+var(WPS$Petal.Width) #setosa
+var(WPV$Petal.Width) #versicolor
+var(WPVI$Petal.Width) #virginica
+#histogram
+par(mfrow = c(1,3))
+hist(WPS$Petal.Width, main = "Petal Width: \n Setosa", xlab = "Width (cm)")
+hist(WPV$Petal.Width, main = "Petal Width: \n Versicolor", xlab = "Width (cm)")
+hist(WPVI$Petal.Width, main = "Petal Width: \n Virginica", xlab = "Width (cm)")
